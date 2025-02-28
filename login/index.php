78365dd10d6f40bceb5c6bb3c20504dfcc568889
DeBeukeleerMaarten <r0986041@student.thomasmore.be>
2025-02-28 09:58:54 +0100

    login page database verbonden

--- login/index.php
+++ login/index.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+
+$errors = [
+  'login' => $_SESSION['login_error'] ?? '',
+  'register' => $_SESSION['register_error'] ?? ''
+];
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error){
+  return !empty($error) ? "<p class='error-message'>$error</p>" : '';
+}
+function isActiveForm($formName,$activeForm){
+  return $formName === $activeForm ? 'active' : '';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,22 +24,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
     <title>Anti-Foodwaste</title>
+    <script src="script.js"></script>
   </head>
   <body>
     <div class="container">
-      <div class="form-box active" id="Login-form">
-        <form action="">
+      <div class="form-box <?= isActiveForm('login', $activeForm) ?>" id="Login-form">
+        <form action="login_register.php" method="post">
           <h2>Login</h2>
+          <?= showError($errors['login']); ?>
           <input type="email" name="email" placeholder="Email" required />
           <input type="password" name="password" placeholder="Password" required />
           <button type="submit" name="login">Login</button>
           <p>Dont have an account? <a href="#" onclick="showForm('Register-form')">Register</a></p>
         </form>
       </div>
-      <div class="form-box" id="Register-form">
-        <form action="">
+      <div class="form-box <?= isActiveForm('register', $activeForm) ?>" id="Register-form">
+        <form action="login_register.php" method="post">
           <h2>Register</h2>
-          <input type="text" name="username" placeholder="username" required />
+          <?= showError($errors['register']); ?>
+          <input type="text" name="name" placeholder="username" required />
           <input type="email" name="email" placeholder="Email" required />
           <input type="password" name="password" placeholder="Password" required/>
           <button type="submit" name="register">Register</button>
@@ -28,6 +50,6 @@
         </form>
       </div>
     </div>
-    <script src="/login/script.js"></script>
+   
   </body>
 </html>
