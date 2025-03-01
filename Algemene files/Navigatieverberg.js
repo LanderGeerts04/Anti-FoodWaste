e2cbed5ec6e714b185e9b9a9afde495ffc5a6eea
Friezel <r1058004@student.thomasmore.be>
2025-03-01 16:08:02 +0100

    Drag en drop van het weekmenu gefixt

---
+++ Algemene files/Navigatieverberg.js
@@ -0,0 +1,8 @@
+function verberg(){
+    if(document.getElementById("nav").style.display=="none"){
+        document.getElementById("nav").style.display = "block"
+    }
+    else{
+        document.getElementById("nav").style.display="none";
+    }  
+}
