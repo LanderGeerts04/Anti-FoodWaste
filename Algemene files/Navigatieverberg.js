4fee799265cbffc96c92f530155e5ff5a687e82a
Friezel <r1058004@student.thomasmore.be>
2025-03-07 08:24:05 +0100

    Zoekbalk aanpassingen

--- Algemene files/Navigatieverberg.js
+++ Algemene files/Navigatieverberg.js
@@ -1,8 +1,10 @@
 function verberg(){
     if(document.getElementById("nav").style.display=="none"){
         document.getElementById("nav").style.display = "block"
+        document.getElementById("keuzemenu").style.marginLeft="300px";
     }
     else{
         document.getElementById("nav").style.display="none";
+        document.getElementById("keuzemenu").style.marginLeft=0;
     }  
 }
