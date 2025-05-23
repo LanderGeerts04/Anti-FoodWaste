1be20bb4fca876521e5f52f4882dedbb20b8a875
Friezel <r1058004@student.thomasmore.be>
2025-05-23 15:54:33 +0200

    Startpagina layout fixed

--- Startpagina/Startpagina.php
+++ Startpagina/Startpagina.php
@@ -7,28 +7,31 @@
     <title>Startpagina</title>
 </head>
 <body>
-    <div id="header">
-        <h1>Startpagina</h1>
+    <div class="header">
+        <div class="banner">
+            <h1>Startpagina</h1>
+        </div>
+        <div id="login">
+            <a class="knop" onclick="window.location.href='../login/index.php'">Log in</a>
+        </div>
     </div>
-<hr>
-    <div id="login">
-        <a class="knop" onclick="window.location.href='../login/index.php'">Sign In</a>
-    </div>
-    <div id="text">
-        <h3>Welkom op onze website!</h3>
-        <p>Voedselverspilling is een groot probleem, zowel voor het milieu als voor je portemonnee. Vaak belanden producten in de vuilbak simpelweg omdat we vergeten dat ze er zijn, of omdat we meer kopen dan we nodig hebben. Onze website helpt je om hier eenvoudig verandering in te brengen.</p>
-        <p>Met onze slimme tool houd je bij wat er in jouw koelkast ligt en wanneer producten vervallen. Zo krijg je een duidelijk overzicht van wat je nog hebt, en word je op tijd herinnerd aan producten die op moeten. Geen vergeten restjes meer achterin de koelkast - je weet altijd precies wat er nog gebruikt kan worden.</p>
-        <p>Daarnaast biedt de website een handige maaltijdplanner. Je kiest welke gerechten je deze week wilt eten, en het systeem houdt automatisch rekening met wat je al in huis hebt. Op basis daarvan stelt het een boodschappenlijstje samen dat alleen de ontbrekende ingredienten bevat. Dat betekent: efficienter boodschappen doen, minder voedselverspilling en minder onnodige uitgaven.</p>
-        <p>Of je nu alleen woont of voor een heel gezin kookt, onze website helpt je bewuster om te gaan met voedsel. Zo draag je bij aan een duurzame levensstijl, zonder dat het extra moeite kost.</p>
-        <p id="slogan">Kortom: minder verspillen, slimmer plannen en duurzamer leven.</p>
-    </div>
-    <div id="photos">
-        <img class="photo" src="../Startpagina/Photos/Home.png">
-        <img class="photo" src="../Startpagina/Photos/Koelkast2.png">
-        <img class="photo" src="../Startpagina/Photos/Recepten2.png">
-        <img class="photo" src="../Startpagina/Photos/Weekmenu2.png">
-        <img class="photo" src="../Startpagina/Photos/Weekmenu3.png">
-        <img class="photo" src="../Startpagina/Photos/Boodschappenlijst2.png">
+    <div class="inhoud">
+        <div id="text">
+            <h3>Welkom op onze website!</h3>
+            <p>Voedselverspilling is een groot probleem, zowel voor het milieu als voor je portemonnee. Vaak belanden producten in de vuilbak simpelweg omdat we vergeten dat ze er zijn, of omdat we meer kopen dan we nodig hebben. Onze website helpt je om hier eenvoudig verandering in te brengen.</p>
+            <p>Met onze slimme tool houd je bij wat er in jouw koelkast ligt en wanneer producten vervallen. Zo krijg je een duidelijk overzicht van wat je nog hebt, en word je op tijd herinnerd aan producten die op moeten. Geen vergeten restjes meer achterin de koelkast - je weet altijd precies wat er nog gebruikt kan worden.</p>
+            <p>Daarnaast biedt de website een handige maaltijdplanner. Je kiest welke gerechten je deze week wilt eten, en het systeem houdt automatisch rekening met wat je al in huis hebt. Op basis daarvan stelt het een boodschappenlijstje samen dat alleen de ontbrekende ingredienten bevat. Dat betekent: efficienter boodschappen doen, minder voedselverspilling en minder onnodige uitgaven.</p>
+            <p>Of je nu alleen woont of voor een heel gezin kookt, onze website helpt je bewuster om te gaan met voedsel. Zo draag je bij aan een duurzame levensstijl, zonder dat het extra moeite kost.</p>
+            <p id="slogan">Kortom: minder verspillen, slimmer plannen en duurzamer leven.</p>
+        </div>
+        <div id="photos">
+            <img class="photo" src="../Startpagina/Photos/Home.png">
+            <img class="photo" src="../Startpagina/Photos/Koelkast2.png">
+            <img class="photo" src="../Startpagina/Photos/Recepten2.png">
+            <img class="photo" src="../Startpagina/Photos/Weekmenu2.png">
+            <img class="photo" src="../Startpagina/Photos/Weekmenu3.png">
+            <img class="photo" src="../Startpagina/Photos/Boodschappenlijst2.png">
+        </div>
     </div>
 </body>
 </html>
